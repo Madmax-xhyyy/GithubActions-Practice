56030b23102bdf8c3cc90524365fc1a2b1c9a3e9
mdmx <143160694+Madmax-xhyyy@users.noreply.github.com>
2026-06-19 19:40:43 +0800

    update package and path

--- api/jest.config.js
+++ api/jest.config.js
@@ -3,7 +3,6 @@ import { createDefaultPreset } from 'ts-jest'
 const tsJestTransformCfg = createDefaultPreset().transform
 
 /** @type {import("jest").Config} **/
-// eslint-disable-next-line no-undef
 export default {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
